230ff6b8c231083df5bf30db69a1e558ad715308
Neeraj J <neerajj6002@gmail.com>
2025-08-03 22:22:21 +0530

    Update home.js

--- home.js
+++ home.js
@@ -6,3 +6,16 @@ window.addEventListener("scroll", function() {
     navbar.classList.remove("scrolled");
   }
 });
+
+const menuToggle = document.getElementById('menu-toggle');
+const sideMenu = document.getElementById('side-menu');
+const closeMenu = document.getElementById('close-menu');
+
+menuToggle.addEventListener('click', () => {
+  sideMenu.classList.add('active');
+});
+
+closeMenu.addEventListener('click', () => {
+  sideMenu.classList.remove('active');
+});
+
